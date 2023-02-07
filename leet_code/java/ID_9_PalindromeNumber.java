18a046922d7bcd6cf10a5e1636001cc09e5bc22d
Passakorn <boss4848988@gmail.com>
2023-02-07 18:38:49 +0700

    [ADD] Palindrome Number

---
+++ leet_code/java/ID_9_PalindromeNumber.java
@@ -0,0 +1,23 @@
+package leet_code.java;
+
+public class ID_9_PalindromeNumber {
+    public static void main(String[] args) {
+        int x = 123454321;
+        System.out.println(isPalindrome(x));
+    }
+
+    public static boolean isPalindrome(int x) {
+        String str_x = Integer.toString(x);
+        char[] leftToRight = str_x.toCharArray();
+        int checkPalindrome = 0;
+        for (int i = 0; i < str_x.length(); i++) {
+            if (str_x.charAt(i) == leftToRight[(str_x.length() - 1) - i]) {
+                checkPalindrome++;
+            }
+        }
+        if (checkPalindrome == str_x.length()) {
+            return true;
+        }
+        return false;
+    }
+}
